a452d926f34f1c8ebdd6d13078d14fbef16f400c
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2012-01-09 16:28:12 +0000

    working with the hex coordinate system for the board. A module named hexmanager will be used to handle the hex grid for the gui. It is used to translate hex to rect coordinate and to find the hex containing the coordinates of the mouse pointer.

--- Eclipse/src/gui/play.py
+++ Eclipse/src/gui/play.py
@@ -8,7 +8,7 @@ from cocos.layer.base_layers import Layer
 from cocos.text import Label
 from cocos.director import director
 from cocos.scenes.transitions import FadeTRTransition, FadeBLTransition,\
-    SlideInBTransition, SlideInTTransition
+    SlideInBTransition, SlideInTTransition, FadeUpTransition, FadeDownTransition
 from cocos.layer.util_layers import ColorLayer
 from cocos.tiles import HexMapLayer, HexCell, Tile, Resource
 from cocos import tiles
@@ -17,6 +17,7 @@ import math
 from cocos.layer.scrolling import ScrollableLayer, ScrollingManager
 import pyglet
 from cocos.sprite import Sprite
+from hexmanager import HexManager
 
 class BoardLayer(ScrollableLayer):
     is_event_handler = True
@@ -28,10 +29,47 @@ class BoardLayer(ScrollableLayer):
         ws = director.get_window_size()
         self.add(Sprite('milkyway.jpg', scale = 0.1, position = (500,500)))
         
+        self.hex_manager = HexManager(20)
+        
     def on_mouse_press (self, x, y, buttons, modifiers):        
         x, y = self.scroller.pixel_from_screen(x,y)
         #self.scroller.set_focus(x,y)
         
+    def on_mouse_motion(self, x, y, dx, dy):    
+        x, y = self.scroller.pixel_from_screen(x,y)
+        print x, y
+        hex = self.hex_manager.get_hex_from_rect_coord(x, y)
+        hex_x, hex_y = self.hex_manager.get_rect_coord_from_hex(hex[0], hex[1])
+        print hex_x, hex_y
+        for child in self.get_children():
+            if isinstance(child, Line):
+                child.kill()
+        self.add_hex((hex_x, hex_y), 20)
+        
+        
+    def add_hex(self, centre, r):        
+        hex_coord = []
+        hex_centre = centre
+        hex_r = r
+        hex_coord.append((hex_centre[0] + hex_r/2,      hex_centre[1] + math.sqrt(3)*hex_r/2))
+        hex_coord.append((hex_centre[0] + hex_r,        hex_centre[1] + 0))
+        hex_coord.append((hex_centre[0] + hex_r/2,      hex_centre[1] - math.sqrt(3)*hex_r/2))
+        hex_coord.append((hex_centre[0] - hex_r/2,      hex_centre[1] - math.sqrt(3)*hex_r/2))
+        hex_coord.append((hex_centre[0] - hex_r,        hex_centre[1] + 0))
+        hex_coord.append((hex_centre[0] - hex_r/2,      hex_centre[1] + math.sqrt(3)*hex_r/2))        
+        w = 3        
+        line1 = Line(hex_coord[0], hex_coord[1],(255,255,255,255) , w)
+        line2 = Line(hex_coord[1], hex_coord[2],(255,255,255,255) , w)
+        line3 = Line(hex_coord[2], hex_coord[3],(255,255,255,255) , w)
+        line4 = Line(hex_coord[3], hex_coord[4],(255,255,255,255) , w)
+        line5 = Line(hex_coord[4], hex_coord[5],(255,255,255,255) , w)
+        line6 = Line(hex_coord[5], hex_coord[0],(255,255,255,255) , w)
+        self.add(line1, 2)
+        self.add(line2, 2)
+        self.add(line3, 2)
+        self.add(line4, 2)
+        self.add(line5, 2)
+        self.add(line6, 2)   
     def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
         self.scroller.scale += scroll_y * 0.1
         self.scroller.scale = min(max(self.scroller.scale, 0.2), 2)
@@ -75,9 +113,9 @@ class ControlLayer(Layer):
         
     def on_key_press(self, key, modifiers):
         if key == 112:
-            director.replace(SlideInTTransition(self.control_list[1], duration = 0.2))
+            director.replace(FadeDownTransition(self.control_list[1], duration = 0.2))
         elif key == 98:
-            director.replace(SlideInBTransition(self.control_list[0], duration = 0.2))
+            director.replace(FadeUpTransition(self.control_list[0], duration = 0.2))
         
 class BoardScene(Scene):
     def __init__(self, control_layer):
